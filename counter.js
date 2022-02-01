d51050c1f079673ce43b614de351dedd137e52fe
rdx6969 <82794243+rdx6969@users.noreply.github.com>
2022-02-01 19:27:51 +0530

    upload'

---
+++ counter.js
@@ -0,0 +1,30 @@
+document.getElementById('footer').innerHTML =
+    `
+    <a id="button" class="p-2 back-to-top" href="#"><i class="far fa-arrow-alt-circle-up"></i></a>
+<footer class="container-fluid border-none">
+<div class="waves">
+    <div class="wave" id="wave1"></div>
+    <div class="wave" id="wave2"></div>
+    <div class="wave" id="wave3"></div>
+    <div class="wave" id="wave4"></div>
+</div>
+<div class="social-media row mt-5 ">
+   <div class="col-3 overflow-hidden">
+   <a href=""><i class="fab fa-whatsapp top-logo"></i></a>
+    </div>
+   <div class="col-3 overflow-hidden">
+   <a href=""><i class="fab fa-instagram top-logo"></i></a>
+    </div>
+   <div class="col-3  overflow-hidden">
+   <a href=""><i class="fab fa-telegram-plane top-logo"></i></a>
+    </div>
+   <div class="col-3 overflow-hidden">
+   <a href=""><i class="fab fa-twitch top-logo"></i></a>
+    </div>
+</div>
+
+<p class="mt-5 "> Copyrights &copy;2022 All Rights Received | This Template is Made By &#9755;
+    <font class="text-info">Sanjay</font>
+</p>
+</footer>
+`
